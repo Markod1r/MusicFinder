26bc2a8914de6c01b7cc10868bece7583fc8d8a4
marcoismakalew <marcoismakalew@gmail.com>
2025-05-28 18:02:31 +0800

    fix: API outputs

--- app.js
+++ app.js
@@ -4,20 +4,16 @@ const sum = document.getElementById("result");
 
 btn.addEventListener("click", () => {
 	const query = input.value;
+	result.innerHTML = "";
 
 	fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(query)}`)
 		.then((res) => res.json())
 		.then((data) => {
 			sum.innerHTML = "";
-			const judulUnik = new Set(); // Set untuk menyimpan judul unik
-
 			data.results.forEach((item) => {
-				if (item.trackName && !judulUnik.has(item.trackName)) {
-					judulUnik.add(item.trackName); // Tambahkan ke set
-					const newElement = document.createElement("h4");
-					newElement.textContent = item.trackName;
-					sum.appendChild(newElement);
-				}
+				const newElement = document.createElement("h4");
+				newElement.textContent = item.trackName;
+				result.appendChild(newElement);
 			});
 		})
 		.catch(() => {
@@ -26,4 +22,3 @@ btn.addEventListener("click", () => {
 
 	input.value = "";
 });
-
