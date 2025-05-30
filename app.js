b12776b1375db107b9093e93092cfe3fba8b4784
marcoismakalew <marcoismakalew@gmail.com>
2025-05-30 15:11:08 +0800

    create: class

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const input = document.getElementById("input");
 const btn = document.getElementById("button");
 const sum = document.getElementById("result");
 
+class Api {
+	constructor(url) {
+		this.url = url;
+	}
+}
+
 btn.addEventListener("click", () => {
 	result.innerHTML = "";
+	const itunes = new Api(`https://itunes.apple.com/search?term=${encodeURIComponent(input.value)}`);
 
-	fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(input.value)}`)
+	fetch(itunes.url)
 		.then((res) => res.json())
 		.then((data) => {
 			sum.innerHTML = "";
 			data.results.forEach((item) => {
 				const newElement = document.createElement("h4");
-				newElement.textContent = item.trackName;
+				newElement.textContent = item.trackName + " - " + item.artistName;
 				result.appendChild(newElement);
 			});
 		})
